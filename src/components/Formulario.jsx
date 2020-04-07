256787f24e8514ff557e6cd3b6a9af42b03817c1
freilinjb <freilinjb@gmail.com>
2020-04-06 21:52:41 -0400

    agrergando Logica al formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from '@emotion/styled';
 
 const Formulario = () => {
+
+    const [ datos, setDatos ] = useState({
+        marca:'',
+        anio:'',
+        plan:'basico'
+    });
+
+    const { marca, anio, plan } = datos;
+
+    //Leer los datos del formulario y colocarlo en el state
+    const obtenerInformacion = e => {
+        setDatos({
+            ...datos,
+            [e.target.name] : e.target.value
+        })
+    }
+
     return ( 
         <form>
-            <div class="form-group">
+            <div className="form-group">
                 <label htmlFor="marca">Marca</label>
-                <select className="form-control" name="marca">
-                    <option disabled selected value="">-- Seleccione --</option>
+                <select className="form-control" name="marca" value={marca} onChange={obtenerInformacion}>
+                    <option disabled defaultValue value="">-- Seleccione --</option>
                     <option value="americano">Americano</option>
                     <option value="europeo">Europeo</option>
                     <option value="asiatico">Asiatico</option>
@@ -15,27 +33,26 @@ const Formulario = () => {
 
             <div className="form-group">
                 <label htmlFor="anio">Año</label>
-                <select className="form-control" name="anio">
-                <option disabled selected value="">-- Seleccione --</option>
-                <option value="">-- Seleccione --</option>
-                <option value="2020">2020</option>
-                <option value="2019">2019</option>
-                <option value="2018">2018</option>
-                <option value="2017">2017</option>
-                <option value="2016">2016</option>
-                <option value="2015">2015</option>
-                <option value="2014">2014</option>
-                <option value="2013">2013</option>
-                <option value="2012">2012</option>
+                <select className="form-control" name="anio" value={anio} onChange={obtenerInformacion}>
+                    <option disabled defaultValue value="">-- Seleccione --</option>
+                    <option value="2020">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2018">2018</option>
+                    <option value="2017">2017</option>
+                    <option value="2016">2016</option>
+                    <option value="2015">2015</option>
+                    <option value="2014">2014</option>
+                    <option value="2013">2013</option>
+                    <option value="2012">2012</option>
                 </select>
             </div>
                
             <div className="form-check form-check-inline">
-                <input className="form-check-input" type="radio" name="plan" id="basico" value="basico"/>
-                <label className="form-check-label" htmlFor="basico"></label>
+                <input className="form-check-input" type="radio" name="plan" id="basico" value="basico" checked={plan === "basico"} onChange={obtenerInformacion}/>
+                <label className="form-check-label" htmlFor="basico">Básico</label>
             </div>
             <div className="form-check form-check-inline">
-                <input className="form-check-input" type="radio" name="plan" id="completo" value="completo"/>
+                <input className="form-check-input" type="radio" name="plan" id="completo" value="completo" checked={plan === "completo"} onChange={obtenerInformacion}/>
                 <label className="form-check-label" htmlFor="completo">Completo</label>
             </div>
         </form>
