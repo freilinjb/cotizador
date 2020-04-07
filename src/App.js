e0c7a5945565a8434278f9d28c3fb075c7ea8f7a
freilinjb <freilinjb@gmail.com>
2020-04-06 20:50:59 -0400

    styled COmponent Formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 import './App.css';
 
+const Contenedor = styled.div`
+  max-width:700px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
+
 function App() {
-  return (
+  return ( 
+    <Contenedor>
       <Header titulo = "Cotizador de seguros"/>
-      <Formulario/>
+      <ContenedorFormulario>
+        <Formulario/>
+      </ContenedorFormulario>
+    </Contenedor>
+
   );
 }
 
